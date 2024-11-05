2dd83572fb836db9c18ecf408e223ca441487266
정곤 <jeong-gon@jeong-gon-ui-MacBookPro.local>
2024-11-05 22:06:34 +0900

    feat: mdx파일 리스트 렌더링, 파일 내용 렌더링

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,22 +1,25 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import Title from './components/title';
 
-export default async function BlogPost({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default async function Post({ params }: { params: { slug: string } }) {
   console.log('process');
   console.log('process', process.cwd());
   const filePath = path.join(
     process.cwd(),
-    'src',
-    'app',
-    'blog',
+    'public',
+    'posts',
     `${params.slug}.mdx`
   );
   const source = await fs.readFile(filePath, 'utf8');
 
-  return <MDXRemote source={source} />;
+  return (
+    <MDXRemote
+      source={source}
+      components={{
+        Title: () => <Title title={params.slug} />,
+      }}
+    />
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import withMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['next-mdx-remote'],
-
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
